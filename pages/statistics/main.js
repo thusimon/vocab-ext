b52f65b69ebfa36a5c152755b615c727f633e234
Lu.Xu <thusimon@gmail.com>
2020-11-15 16:24:32 -0800

    fix d3 bugs

--- pages/statistics/main.js
+++ pages/statistics/main.js
@@ -3,7 +3,7 @@
 const controllerE = document.getElementById('statistics-controller');
 const chartE = document.getElementById('statistics-charts');
 
-const chartCaption = chrome.i18n.getMessage('stat_vocab_add_header');
+const chartTitle = chrome.i18n.getMessage('stat_vocab_add_header');
 const dataOptionWeekE = document.getElementById('data-source-option-week');
 const dataOptionMonthE = document.getElementById('data-source-option-month');
 const dataOptionQuarterE = document.getElementById('data-source-option-quarter');
@@ -44,8 +44,8 @@ const getCountTimeStat = (vocab) => {
     currentDay = currentDayDate.getTime();
   }
   // convert stat to array
-  const statArr = Object.keys(stat).map(d=>+d).map((dayTime) => {
-    return {day: dayTime, count: stat[dayTime]}
+  const statArr = Object.keys(stat).map((dayTime) => {
+    return {key: +dayTime, value: stat[dayTime]}
   });
 
   return statArr;
@@ -79,7 +79,7 @@ const getYearData = (data) => {
 
 const getDataByTimeRange = (timeRange) => {
   const countTimeStat = getCountTimeStat(vocab);
-  countTimeStat.sort((v1, v2) => v1.day - v2.day);
+  countTimeStat.sort((v1, v2) => v1.key - v2.key);
   switch (timeRange) {
     case 'week':
       return getWeekData(countTimeStat)
@@ -110,157 +110,32 @@ const config = {
   width: 1000,
   height: 850,
   chartMargin: [50, 30, 30, 30],
-  chargBg: 'lightyellow',
+  chartBg: 'lightyellow',
   chartWidth: 1000,
   chartHeight: 850,
   barColor: '#1034a6',
   barHoverColor: '#0080ff',
-  tooltipColor: 'green'
-}
-
-const updateConfig = () => {
-  config.width = window.innerWidth / 1.1;
-  config.height = window.innerHeight / 1.2 > 600 ? 600 : window.innerHeight / 1.2;
-  config.chartHeight = config.height - config.chartMargin[0] - config.chartMargin[2];
-  config.chartWidth = config.width - config.chartMargin[1] - config.chartMargin[3];
-}
-
-updateConfig();
-
-const svg = d3.select('#statistics-charts')
-  .append('svg')
-  .attr('width', config.width)
-  .attr('height', config.height);
-
-const updateChart = (data, chartCaption, extendXRange) => {
-  const barWidth = config.chartWidth/data.length / 1.2;
-
-  const xExtent = d3.extent(data, d => d.day);
-  // extend one day to both left and right
-  const yExtent = d3.extent(data, d => d.count);
-
-  if (extendXRange) {
-    xExtent[0] -= ONE_DAY_MS;
-    xExtent[1] += ONE_DAY_MS
-  }
-
-  if (yExtent[1] == 0) {
-    yExtent[1] = 10;
-  }
-
-  const scaleX = d3.scaleBand()
-    .domain(data.map(d => d.day))
-    .range([0, config.chartWidth]);
-  
-  const scaleXLinear = d3.scaleLinear()
-    .domain([xExtent[0], xExtent[1]])
-    .range([0, config.chartWidth]);
-
-  const scaleY = d3.scaleLinear()
-    .domain([yExtent[0], yExtent[1]*1.05])
-    .range([config.chartHeight, 0]);
-
-  const x_axis = d3.axisBottom()
-    .scale(scaleX)
-    .tickValues(scaleX.domain().filter(function(d,i){ return !(i%10)}))
-    .tickFormat(d => {
-      const date = new Date(d);
-      return `${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`;
-    });
-
-  const y_axis = d3.axisLeft()
-    .scale(scaleY);
-  
-  const yTicks = scaleY.ticks();
-  yTicks.shift();
-
-  const chart = svg.append("g")
-    .attr('transform', `translate(${config.chartMargin[3]}, ${config.chartMargin[0]})`)
-
-  const backgroundRect = chart.append('rect')
-    .attr('width', config.chartWidth)
-    .attr('height', config.chartHeight)
-    .attr('fill', config.chargBg)
-    .attr('opacity', 0.5);
-
-  // add dashed area on Y axis
-  const ydashLines = yTicks.map(y => [[xExtent[0], y], [xExtent[1], y]])
-  const dashedAreaLines = chart.selectAll('.y-dash-area')
-    .data(ydashLines)
-    .enter()
-    .append('line')
-    .attr('x1', d=>scaleXLinear(d[0][0]))
-    .attr('y1', d=>scaleY(d[0][1]))
-    .attr('x2', d=>scaleXLinear(d[1][0]))
-    .attr('y2', d=>scaleY(d[1][1]))
-    .attr('class', 'y-dash-area');
-
-  const tooltip = chart.append("text")
-    .attr('id', 'tooltip')
-    .style('opacity', 1)
-    .style('font-weight', 600)
-    .style('fill', config.tooltipColor)
-    .text("");
-
-  const rects = chart.selectAll('rect')
-    .data(data)
-    .enter()
-    .append('rect')
-    .attr('x', (d) => scaleX(d.day) - barWidth/2)
-    .attr('y', config.chartHeight)
-    .attr('width', barWidth)
-    .attr('height', 0)
-    .attr('fill', config.barColor)
-    .on('mouseover', function (evt, d) {
-      const date = new Date(d.day);
-      const msg = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}: ${d.count}`;
-      const x = scaleXLinear(d.day)-40;
-      const y = scaleY(d.count) - 10;
-      d3.select(this).transition()
-        .duration(100)
-        .attr('fill', config.barHoverColor);
-      d3.selectAll('#tooltip').transition()
-        .duration(200)
-        .text(msg)
-        .attr('x', x)
-        .attr('y', y)
-        .style('opacity', 1)
-    })
-    .on('mouseout', function (evt, d) {
-      d3.select(this).transition()
-        .duration(100)
-        .attr('fill', config.barColor);
-      d3.selectAll('#tooltip').transition()
-        .duration(200)
-        .text('')
-        .style('opacity', 0)
-    });
-
-  svg.append('g')
-    .attr("transform", `translate(${config.chartMargin[3]}, ${config.chartMargin[0]})`)
-    .call(y_axis);
-
-  svg.append('g')
-    .attr("transform", `translate(${config.chartMargin[3]}, ${config.chartHeight + config.chartMargin[0]})`)
-    .call(x_axis);
-
-  svg.append('text')
-    .attr('x', config.width / 2)           
-    .attr('y', config.chartMargin[0]/2)
-    .attr('text-anchor', 'middle')  
-    .style('font-size', '30px') 
-    .style('font-weight', '600')  
-    .text(chartCaption);
-
-  rects.transition()
-    .duration(750)
-    .attr('x', (d) => scaleX(d.day) - barWidth/2)
-    .attr('y', (d) => scaleY(d.count))
-    .attr('width', barWidth)
-    .attr('height', (d) => config.chartHeight - scaleY(d.count));
+  tooltipColor: 'green',
+  title: chartTitle
 }
 
 const dataToDraw = getDataByTimeRange('all');
-updateChart(dataToDraw, chartCaption);
+
+const customization = {
+  xTickFormat: (d) => {
+    const date = new Date(d);
+    return `${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`;
+  },
+  xTickValue: (d, i) => !(i%7),
+  yTickFormat: d => {
+    if (d%5 == 0) {
+      return d;
+    } else {
+      return null;
+    }
+  }
+}
+const barChart = new BarChart('#statistics-charts', config, [], customization);
+barChart.updateVis(dataToDraw);
 
 })();
