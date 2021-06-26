c4a075f778ec2727ebc55626d2ad5af3134461de
Lu Xu <thusimon@gmail.com>
2021-06-26 13:15:39 -0700

    improve the new tab experience

--- pages/new-tab/main.js
+++ pages/new-tab/main.js
@@ -3,14 +3,17 @@ const vocabDisplayE = document.getElementById('vocab-display')
   , refreshBtnE = document.getElementById('refresh-btn')
   , originalVocabE = document.getElementById('vocab-original')
   , dictE = document.getElementById('dict')
-  , translationE = document.getElementById('translation');
+  , translationE = document.getElementById('translation')
+  , totalCountE = document.getElementById('total-count');
 
-const getRandomVocabulary = async () => {
-  let {SOURCE_LANG, TARGET_LANG} = await storageGetP(STORAGE_AREA.SETTINGS, DEFAULT_SETTING);
+const getVocabs = async () => {
+  const {SOURCE_LANG, TARGET_LANG} = await storageGetP(STORAGE_AREA.SETTINGS, DEFAULT_SETTING);
   const vocabs = await storageGetP(STORAGE_AREA.VOCAB, {});
   const vocabTranslateArea = `${SOURCE_LANG}-${TARGET_LANG}`;
-  const vocabsWithSetting = vocabs[vocabTranslateArea] || {};
-  
+  return vocabs[vocabTranslateArea] || {};
+}
+
+const getRandomVocabulary = (vocabsWithSetting) => {
   const vocabsKey = Object.keys(vocabsWithSetting)
 
   if (vocabsKey.length > 0) {
@@ -21,18 +24,46 @@ const getRandomVocabulary = async () => {
   return null;
 }
 
+refreshBtnE.addEventListener('click', async () => {
+  const vocabs = await getVocabs()
+  const randomVocab = getRandomVocabulary(vocabs);
+  constructNewTabVocab(randomVocab);
+});
+
 const constructNewTabVocab = (vocab) => {
-  const {dict, createdTime, original, translation} = vocab;
-  
+  const {dict, original, translation} = vocab;
+  originalVocabE.textContent = original;
+  translationE.textContent = translation;
+  while (dictE.firstChild) {
+    dictE.removeChild(dictE.firstChild);
+  }
+  if (dict && dict.length > 0) {
+    dict.forEach(d => {
+      const oneDictE = document.createElement('div');
+      oneDictE.className = 'dict-item';
+      const typeE = document.createElement('span');
+      typeE.className = 'dict-type';
+      typeE.textContent = `[${d.pos}]`;
+      const dictContentE = document.createElement('span');
+      dictContentE.className = 'dict-content';
+      dictContentE.textContent = d.terms.join(', ');
+      oneDictE.append(typeE, dictContentE);
+      dictE.append(oneDictE);
+    })
+  }
 }
 
 (async () => {
-  const randomVocab = await getRandomVocabulary();
+  const vocabs = await getVocabs()
+  const randomVocab = getRandomVocabulary(vocabs);
   if (randomVocab) {
     vocabDisplayE.style.display = 'block';
     vocabWelcomeE.style.display = 'none';
+    constructNewTabVocab(randomVocab);
   } else {
     vocabDisplayE.style.display = 'none';
     vocabWelcomeE.style.display = 'block';
   }
+  const vocabsLen = Object.keys(vocabs).length
+  totalCountE.textContent = `Total: ${vocabsLen}`
 })()
