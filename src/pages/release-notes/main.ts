51f9a09dedc7d72f8c769af293815d9e2562d176
Lu Xu <thusimon@gmail.com>
2023-05-31 20:45:59 -0700

    improve release-notes

--- src/pages/release-notes/main.ts
+++ src/pages/release-notes/main.ts
@@ -1,1 +1,22 @@
-console.log('release notes');
+const clickHereE = document.getElementsByClassName('click-here');
+const exampleContainerE = document.getElementById('example-container');
+const imageE = document.getElementById('example-img') as HTMLImageElement;
+const closeE = document.getElementById('example-close')
+
+const clickHereHander = (evt: MouseEvent) => {
+  const target = evt.target as HTMLElement;
+  const img = target.dataset.img;
+  if(!img) {
+    return;
+  }
+  imageE.src = `images/${img}.gif`;
+  exampleContainerE.className = 'show';
+}
+
+for(const element of clickHereE){
+  element.addEventListener('click', clickHereHander);
+}
+
+closeE.addEventListener('click', () => {
+  exampleContainerE.className = 'hide';
+});
