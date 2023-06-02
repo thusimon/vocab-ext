8a6341a22109945986e1d6aebf19435f401916fa
Lu Xu <thusimon@gmail.com>
2023-06-01 20:51:06 -0700

    calculate the last version in view port

--- src/pages/release-notes/main.ts
+++ src/pages/release-notes/main.ts
@@ -1,7 +1,14 @@
+import { debounce } from "../../common/utils";
+
 const clickHereE = document.getElementsByClassName('click-here');
 const exampleContainerE = document.getElementById('example-container');
 const imageE = document.getElementById('example-img') as HTMLImageElement;
-const closeE = document.getElementById('example-close')
+const closeE = document.getElementById('example-close');
+const tableE = document.getElementById('release-table');
+const versionsE = document.getElementsByClassName('version');
+const tableCellE = document.querySelector('table tr td');
+const tableCellEPos = tableCellE.getBoundingClientRect();
+const tableCellHeight = tableCellEPos.height;
 
 const clickHereHander = (evt: MouseEvent) => {
   const target = evt.target as HTMLElement;
@@ -20,3 +27,20 @@ for(const element of clickHereE){
 closeE.addEventListener('click', () => {
   exampleContainerE.className = 'hide';
 });
+
+const getLastVersionInView = () => {
+  const tableRect = tableE.getBoundingClientRect();
+  const tableY = tableRect.y;
+  const firstVersionY = tableY + 42;
+  const windowBottom = window.innerHeight + window.scrollY
+  const currentMaxVersionIndex = Math.round((windowBottom - firstVersionY) / tableCellHeight);
+  const currentVersionIndex = Math.min(versionsE.length, currentMaxVersionIndex - 1);
+  console.log(currentVersionIndex);
+}
+
+getLastVersionInView();
+
+window.addEventListener('scroll', debounce(() => {
+  getLastVersionInView();
+}, 500));
+
